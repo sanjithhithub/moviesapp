e30b9cdda81740a7fcbfbe199884458cfc052021
sanjithhithub <msanjith130@gmail.com>
2026-03-11 20:38:08 +0530

    adding database

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py collectstatic --noinput
