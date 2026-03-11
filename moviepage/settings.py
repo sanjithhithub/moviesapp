899604dfc4b8c61dd0d75b073252ac0b20c05008
sanjithhithub <msanjith130@gmail.com>
2026-03-11 20:10:24 +0530

    adding database

--- moviepage/settings.py
+++ moviepage/settings.py
@@ -98,23 +98,22 @@ TEMPLATES = [
 # ===============================
 # DATABASE CONFIGURATION
 # ===============================
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DEBUG:
-    # Local development database (SQLite)
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
-    }
-
-else:
-    # Production database (Supabase PostgreSQL)
-    DATABASES = {
-        "default": dj_database_url.config(
-            default=os.environ.get("DATABASE_URL"),
-            conn_max_age=600,
-        )
     }
 # ===============================
 # PASSWORD VALIDATION
