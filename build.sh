50221e1649c478bae6b2f2f7f97edeb5a5b873e3
sanjithhithub <msanjith130@gmail.com>
2026-03-14 07:18:13 +0530

    chages made in the build

--- build.sh
+++ build.sh
@@ -1,23 +1,7 @@
 #!/usr/bin/env bash
-
-# Stop script if any command fails
 set -o errexit
 
-echo "Updating pip..."
-python -m pip install --upgrade pip
+pip install -r requirements.txt
 
-echo "Installing dependencies..."
-python -m pip install -r requirements.txt
-
-echo "Cleaning old static files..."
-if [ -d "staticfiles" ]; then
-    rm -rf staticfiles
-fi
-
-echo "Collecting static files..."
 python manage.py collectstatic --noinput
-
-echo "Running migrations..."
 python manage.py migrate --noinput
-
-echo "Build completed successfully."
