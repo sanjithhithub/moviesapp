21d5e0edd9a7ac7650ee746197488e222c5615b4
sanjithhithub <msanjith130@gmail.com>
2026-03-14 12:22:37 +0530

    chages made in the settings

--- moviepage/settings.py
+++ moviepage/settings.py
@@ -23,6 +23,8 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     ".onrender.com",
+    "moviesapp-1rl6.onrender.com",
+
 ]
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
